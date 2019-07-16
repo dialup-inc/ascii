fe4d5c59ba3c15f792df0e1a837209ea450a42b8
Max Hawkins <maxhawkins@gmail.com>
2019-07-15 19:00:15 -0700

    more descriptive camera errors on Darwin

--- camera/cam_darwin.go
+++ camera/cam_darwin.go
@@ -29,12 +29,34 @@ type Camera struct {
 	callback func(image.Image)
 }
 
+type CamError int
+
+const (
+	ErrCamOK CamError = 0
+	ErrCamInitFailed CamError = -1
+	ErrCamOpenFailed = -2
+	ErrCamNotFound = -3
+)
+
+func (c CamError) Error() string {
+	switch c {
+	case ErrCamInitFailed:
+		return "init failed"
+	case ErrCamOpenFailed:
+		return "open failed"
+	case ErrCamNotFound:
+		return "not found"
+	default:
+		return fmt.Sprintf("error %d", c)
+	}
+}
+
 func (c *Camera) Start(camID, width, height int) error {
 	c.width = width
 	c.height = height
 
 	if ret := C.cam_start(c.c, C.int(camID), C.int(width), C.int(height)); ret != 0 {
-		return fmt.Errorf("error %d", ret)
+		return CamError(ret)
 	}
 	return nil
 }
@@ -61,7 +83,7 @@ func New(cb func(image.Image)) (*Camera, error) {
 	})
 
 	if ret := C.cam_init(&cam.c, (C.FrameCallback)(unsafe.Pointer(C.onFrame_cgo)), unsafe.Pointer(&cam.handleID)); ret != 0 {
-		return nil, fmt.Errorf("error %d", ret)
+		return nil, fmt.Errorf("error %d", CamError(ret))
 	}
 	return cam, nil
 }
